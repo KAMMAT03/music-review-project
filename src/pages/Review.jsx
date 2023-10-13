cf92fa40c5e3abef2bfab2b995d78b2a520d7673
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-13 22:04:57 +0200

    added diff colors for specific review score

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 
 export default function Review({reviewProps}){
+    function getColor(){
+        let color;
+        if (reviewProps.score < 4){
+            color = "red";
+        } else if (reviewProps.score < 7){
+            color = "yellow";
+        } else{
+            color = "rgb(52, 255, 52)";
+        }
+        return color;
+    }
+
     return (
         <div className="review">
             <div  className="review-header">
@@ -9,7 +21,7 @@ export default function Review({reviewProps}){
                     <h3 className="review-user">By: {reviewProps.username}</h3>
                 </div>
                 <div className="review-score">
-                    <span>{reviewProps.score}</span>
+                    <span style={{color: getColor()}}>{reviewProps.score}</span>
                 </div>
             </div>
             <p className="review-content">{reviewProps.content}</p>
