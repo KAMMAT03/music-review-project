07fdf55489f03eda4139791f3853f04974aaf898
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-13 13:56:51 +0200

    working on sidebar in albumpage

--- src/pages/AlbumPage.jsx
+++ src/pages/AlbumPage.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import Tracks from "./Tracks";
 import '../styles/albumpage.css';
 
 export default function AlbumPage(){
-    return <h1>Hi</h1>
+    const [album, setAlbum] = React.useState({});
+
+    React.useEffect(() => {
+        fetch(`http://localhost:8080/api/albums/18NOKLkZETa4sWwLMIm0UZ`)
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }, [])
+
+    return (
+        <div className="albumpage-main">
+            <div className="albumpage-info">
+                <img
+                    className="albumpage-cover"
+                    src="https://i.scdn.co/image/ab67616d0000b273881d8d8378cd01099babcd44"
+                    alt=""
+                />
+                <h1 className="albumpage-name">UTOPIA</h1>
+                <div className="albumpage-artists">
+                    <p>Travis Scott</p>
+                </div>
+                <p className="albumpage-date">Released: 2023-07-28</p>
+                <p>Tracklist:</p>
+                <div className="albumpage-tracklist">
+                    <Tracks />
+                </div>
+            </div>
+        </div>
+    )
 }
---
+++ src/pages/Tracks.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Tracks(props){
+    return (
+        <div className="tracks">
+            <ol className="tracks-list">
+                <li>
+                    <h3>HYENA</h3>
+                    <p>Travis Scott</p>
+                </li>
+                <li>
+                    <h3>HYENA</h3>
+                    <p>Travis Scott</p>
+                </li>
+                <li>
+                    <h3>HYENA</h3>
+                    <p>Travis Scott</p>
+                </li>
+                <li>
+                    <h3>HYENA</h3>
+                    <p>Travis Scott</p>
+                </li>
+            </ol>
+        </div>
+    )
+}
